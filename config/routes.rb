d92afa8e7b2605114d623099ddd8f1c3e0a9bbdd
lukas noetzel <lukas.noetzel@gmail.com>
2023-03-07 17:01:59 +0100

    Rails_Wikinimous1.1

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :articles
+  # except: [:index]
+  # get 'articles', to: 'articles#index'
 end
